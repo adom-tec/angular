fcb2e7fc439fe085c507f3759d70f4d47ad9c5d4
otniel acevedo <otniel.a.55@gmail.com>
2019-01-16 15:49:12 -0400

    agregar boton descargar reporte a tabla servicios irregulares

--- src/app/components/graphic-report/graphic-report.component.ts
+++ src/app/components/graphic-report/graphic-report.component.ts
@@ -1,42 +1,48 @@
-import { AssignService } from './../../models/assignService';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatPaginator, MatSort, MatDialog } from '@angular/material';
-import { HttpService, AuthenticationService } from '../../services';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { Router } from '@angular/router';
+import { ResponseContentType } from '@angular/http';
 import { NotifierService } from 'angular-notifier';
 import { Observable } from 'rxjs/Observable';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 import 'rxjs/add/observable/forkJoin';
 import * as moment from 'moment';
+import * as FileSaver from 'file-saver';
+
+import { AssignService } from './../../models/assignService';
+import { HttpService, AuthenticationService } from '../../services';
 import { environment } from '../../../environments/environment';
 import { ServiceChartStatus } from './../../models/serviceChartStatus';
 import { PatientReportData } from '../../models/patientReportData';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-graphic-report',
   templateUrl: './graphic-report.component.html',
   styleUrls: ['./graphic-report.component.css']
 })
-export class GraphicReportComponent implements OnInit {
-  public irregularServiceColumns: string[] = [];
-  public irregularServices = new MatTableDataSource([]);
-  public filterIrregularServices: string;
+export class GraphicReportComponent implements OnInit, AfterViewInit, OnDestroy {
+  irregularServiceColumns: string[] = [];
+  irregularServices = new MatTableDataSource([]);
+  filterIrregularServices: string;
 
-  public withoutProfessionalColumns: string[] = [];
-  public withoutProfessional = new MatTableDataSource([]);
-  public filterWithoutProfessional: string;
+  withoutProfessionalColumns: string[] = [];
+  withoutProfessional = new MatTableDataSource([]);
+  filterWithoutProfessional: string;
 
-  public copaymentColumns: string[] = [];
-  public copayments = new MatTableDataSource([]);
-  public filterCopayments: string;
+  copaymentColumns: string[] = [];
+  copayments = new MatTableDataSource([]);
+  filterCopayments: string;
 
-  public mainSpinner: boolean = false;
-  public showChart: boolean = false;
-  public barChartNursingLabels: string[] = [];
-  public barChartNursingData: any[] = [];
-  public barChartTherapyLabels: string[] = [];
-  public barChartTherapyData: any[] = [];
+  mainSpinner: boolean = false;
+  irregServicesSipinner: boolean = false;
+  showChart: boolean = false;
+  barChartNursingLabels: string[] = [];
+  barChartNursingData: any[] = [];
+  barChartTherapyLabels: string[] = [];
+  barChartTherapyData: any[] = [];
   //config
-  public barChartOptions: any = {
+  barChartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true,
     maintainAspectRatio: false,
@@ -47,8 +53,8 @@ export class GraphicReportComponent implements OnInit {
       }]
     }
   };
-  public barChartType: string = 'bar';
-  public barChartLegend: boolean = true;
+  barChartType: string = 'bar';
+  barChartLegend: boolean = true;
 
   @ViewChild('irregularServPager') irregularServPager: MatPaginator;
   @ViewChild('irregularServTable') irregularServTable: MatSort;
@@ -56,6 +62,8 @@ export class GraphicReportComponent implements OnInit {
   @ViewChild('noProfessionalTable') noProfessionalTable: MatSort;
   @ViewChild('copaymentPager') copaymentPager: MatPaginator;
   @ViewChild('copaymentTable') copaymentTable: MatSort;
+
+  private _onDestroy = new Subject<void>();
 
   constructor(
     private http: HttpService,
@@ -73,44 +81,46 @@ export class GraphicReportComponent implements OnInit {
       this.http.get(`${environment.apiUrl}/api/irregularservices`),
       this.http.get(`${environment.apiUrl}/api/professionals/-1/services`), //servicios sin profesional asignado
       this.http.get(`${environment.apiUrl}/api/professionals/copayment`) //copagos sin entregar por profesional
-    ).subscribe(res => {
-      this.mapToChartFormat(res[0].json(), this.barChartNursingData, this.barChartNursingLabels);
-      this.mapToChartFormat(res[1].json(), this.barChartTherapyData, this.barChartTherapyLabels);
+    )
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(res => {
+        this.mapToChartFormat(res[0].json(), this.barChartNursingData, this.barChartNursingLabels);
+        this.mapToChartFormat(res[1].json(), this.barChartTherapyData, this.barChartTherapyLabels);
 
-      this.irregularServices.data = res[2].json().map((patient: PatientReportData) => {
-        return {
-          AssignServiceId: patient.AssignServiceId,
-          PatientId: patient.PatientId,
-          PatientName: patient.PatientName,
-          ServiceName: patient.ServiceName,
-          Reason: patient.Reason
-        }
+        this.irregularServices.data = res[2].json().map((patient: PatientReportData) => {
+          return {
+            AssignServiceId: patient.AssignServiceId,
+            PatientId: patient.PatientId,
+            PatientName: patient.PatientName,
+            ServiceName: patient.ServiceName,
+            Reason: patient.Reason
+          }
+        });
+        this.irregularServiceColumns = this.irregularServices.data.length ? Object.keys(this.irregularServices.data[0]).filter(val => val !== 'PatientId') : [];
+
+        this.withoutProfessional.data = res[3].json().map((assignService: AssignService) => {
+          return {
+            AssignServiceId: assignService.AssignServiceId,
+            PatientId: assignService.PatientId,
+            PatientName: assignService.patient.NameCompleted,
+            ServiceName: assignService.service.Name
+          }
+        });
+        this.withoutProfessionalColumns = this.withoutProfessional.data.length ? Object.keys(this.withoutProfessional.data[0]).filter(val => val !== 'PatientId') : [];
+
+        this.copayments.data = res[4].json().map(pro => {
+          return {
+            NameCompleted: (pro.professional.user.FirstName + ' ' + (pro.professional.user.SecondName || '') + ' ' + pro.professional.user.Surname + ' ' + (pro.professional.user.SecondSurname || '')).trim().toUpperCase(),
+            ReceivedAmount: pro.ReceivedAmount
+          }
+        });
+        this.copaymentColumns = this.copayments.data.length ? Object.keys(this.copayments.data[0]) : [];
+
+        this.mainSpinner = false;
+      }, err => {
+        this.mainSpinner = false;
+        if (err.status === 401) { return; } this.notifier.notify('error', err.status >= 500 ? 'Ha ocurrido un error, por favor comuníquese con el administrador de sistema' : err.json().message ? err.json().message : 'No se pudo obtener la información, por favor recargue la página e intente nuevamente');
       });
-      this.irregularServiceColumns = this.irregularServices.data.length ? Object.keys(this.irregularServices.data[0]).filter(val => val !== 'PatientId') : [];
-
-      this.withoutProfessional.data = res[3].json().map((assignService: AssignService) => {
-        return {
-          AssignServiceId: assignService.AssignServiceId,
-          PatientId: assignService.PatientId,
-          PatientName: assignService.patient.NameCompleted,
-          ServiceName: assignService.service.Name
-        }
-      });
-      this.withoutProfessionalColumns = this.withoutProfessional.data.length ? Object.keys(this.withoutProfessional.data[0]).filter(val => val !== 'PatientId') : [];
-
-      this.copayments.data = res[4].json().map(pro => {
-        return {
-          NameCompleted: (pro.professional.user.FirstName + ' ' + (pro.professional.user.SecondName || '') + ' ' + pro.professional.user.Surname + ' ' + (pro.professional.user.SecondSurname || '')).trim().toUpperCase(),
-          ReceivedAmount: pro.ReceivedAmount
-        }
-      });
-      this.copaymentColumns = this.copayments.data.length ? Object.keys(this.copayments.data[0]) : [];
-
-      this.mainSpinner = false;
-    }, err => {
-      this.mainSpinner = false;
-      if (err.status === 401) { return; } this.notifier.notify('error', err.status >= 500 ? 'Ha ocurrido un error, por favor comuníquese con el administrador de sistema' : err.json().message ? err.json().message : 'No se pudo obtener la información, por favor recargue la página e intente nuevamente');
-    });
   }
 
   ngAfterViewInit() {
@@ -122,8 +132,13 @@ export class GraphicReportComponent implements OnInit {
     this.copayments.sort = this.copaymentTable;
   }
 
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
 
-  public applyFilter(filterValue: string, type: string): void {
+
+  applyFilter(filterValue: string, type: string): void {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
 
@@ -146,12 +161,12 @@ export class GraphicReportComponent implements OnInit {
     }
   }
 
-  public mapToChartFormat(nursing: ServiceChartStatus[], dataSource: any[], labels: string[]): void {
+  mapToChartFormat(nursing: ServiceChartStatus[], dataSource: any[], labels: string[]): void {
     let nursingStatus = [];
 
     //ordena el array por mes y status
     nursing = nursing.sort((a, b) => {
-        return (+a.Month) > (+b.Month) ? 1 : (+a.Month) < (+b.Month) ? -1 : (+a.Status) > (+b.Status) ? -1 : (+a.Status) < (+b.Status) ? 1 : 0;
+      return (+a.Month) > (+b.Month) ? 1 : (+a.Month) < (+b.Month) ? -1 : (+a.Status) > (+b.Status) ? -1 : (+a.Status) < (+b.Status) ? 1 : 0;
     });
 
     //devuelve un array con los status existentes
@@ -192,5 +207,26 @@ export class GraphicReportComponent implements OnInit {
         assignServiceId: assignServiceId
       }
     });
+  }
+
+  /**
+ * downloadReport
+ */
+  downloadReport() {
+    this.irregServicesSipinner = true;
+
+    this.http.get(`${environment.apiUrl}/api/reports/irregularservices`, {
+      responseType: ResponseContentType.Blob//requerido para que la response sea un blob
+    })
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(res => {
+        FileSaver.saveAs(new Blob([res.blob()], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }), `Reporte de servicios irregulares ${moment(Date.now()).format('DD-MM-YYYY')}.xlsx`);
+
+        this.notifier.notify('success', 'Se ha descargado un excel con la información solicitada');
+        this.irregServicesSipinner = false;
+      }, err => {
+        this.irregServicesSipinner = false;
+        if (err.status === 401) { return; } this.notifier.notify('error', err.status >= 500 ? 'Ha ocurrido un error, por favor comuníquese con el administrador de sistema' : err.json().message ? err.json().message : 'No se pudo obtener la información, por favor recargue la página e intente nuevamente');
+      });
   }
 }
